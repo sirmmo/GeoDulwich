d2be49dca08b39f3d1adaecd1a6ecdebc29dfc6e
Marco Montanari <sirmmo@gmail.com>
2011-09-22 16:22:52 +0200

    major changes

---
+++ geodulwich/backends/object_stores/__init__.py
@@ -0,0 +1,50 @@
+from binascii import hexlify, unhexlify
+import os
+import tempfile
+import time
+import zlib
+
+import threading
+from Queue import Queue
+
+# for the object store
+from dulwich.object_store import BaseObjectStore, ShaFile, ObjectStoreIterator
+from dulwich.objects import Blob
+from dulwich.pack import PackData, iter_sha1, write_pack_index_v2, Pack, load_pack_index_file
+from cStringIO import StringIO
+
+from pymongo import Connection
+
+# for the refstore
+from dulwich.repo import RefsContainer, SYMREF
+
+# for the repo
+from dulwich.repo import BaseRepo
+
+import logbook
+log = logbook.Logger('geogit')
+
+
+class GeoObjectStore(BaseObjectStore):
+    
+    def contains_packed(self, sha):
+        """Check if a particular object is present by SHA1 and is packed."""
+        return False
+    
+    @property
+    def packs(self):
+        """List with pack objects."""
+        return []
+    
+    def _stringify(self,  obj):
+        content = pickle.dumps(obj)
+        content = base64.b64encode(content)
+        
+        return content
+        
+    def _unstringify(self, string):
+        content = base64.b64decode(string)
+        content = pickle.loads(content)
+        
+        return StringIO(content)
+        
--- geodulwich/georepo.py
+++ geodulwich/georepo.py
@@ -27,11 +27,30 @@ log = logbook.Logger('geogit')
 from geodulwich.backends.refs_containers.redis import RefsContainerImplementation
 from geodulwich.backends.object_stores.mongo import ObjectStoreImplementation
 
-class MongoGeoRepo(BaseRepo):
+import pickle
+
+
+
+class MongoRedisGeoRepo(BaseRepo):
     def __init__(self, db_name, store_name, refs_name, host="localhost", port=27017):
         self.connection = Connection(host, port)
         self.db = connection[db_name]
         object_store = MongoObjectStore(self.db[store_name])
+        self._named_files = connection["%s-%s" % (db_name, "named_files", )]
         refs = RedisRefsContainer(self.db[refs_name])
         
         super(MongoRepo, self).__init__(object_store, refs)
+        
+    def _put_named_file(self, path, contents):
+        content = self._stringify(contents)
+        
+        self._named_files.save({'_id':path, "content":content})
+        
+    def get_named_file(self, path):
+        file = self._named_files.find_one({'_id':path})
+        if file:
+            return self._unstringify(file['content'])
+        return None
+    
+    def open_index(self):
+        raise NoIndexPresent()
